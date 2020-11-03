312b6cbe4906f97007c1b1f6a0ecda20c34125ff
Eamon <backtym@live.cn>
2020-11-03 13:47:32 +0800

    引入A.CTable实现根据entity自动创建更新DB

--- src/main/java/com/tangym/artemis/ArtemisApplication.java
+++ src/main/java/com/tangym/artemis/ArtemisApplication.java
@@ -4,11 +4,15 @@ import com.tangym.artemis.annotation.EnableGlobalDispose;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 /**
  * @author backtym@live.cn
  */
-@MapperScan("com.tangym.artemis.mapper") //不明确到mapper层级 报错Invalid bound statement (not found)
+@MapperScan({"com.tangym.artemis.mapper", "com.gitee.sunchenbin.mybatis.actable.dao.*"})
+//不明确到mapper层级 报错Invalid bound statement (not found)
+@ComponentScan({"com.gitee.sunchenbin.mybatis.actable.manager.*", "com.tangym.artemis.*"})
 @EnableGlobalDispose
 @SpringBootApplication
 public class ArtemisApplication {
